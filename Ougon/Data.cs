15ca84589636ee624fc35dc45e89fe9e50645562
Sky <sky@leite.dev>
2023-11-07 11:55:32 -0300

    Add rudimentary move editing

--- Ougon/Data.cs
+++ Ougon/Data.cs
@@ -45,32 +45,139 @@ namespace Ougon
         [FieldOffset(0x0)]
         public short team;
 
-        // Very possibly an array somehow?
         [FieldOffset(0xC)]
-        public Move* currentMove;
+        public Frame* currentAnimationFrame;
 
         [FieldOffset(0x144)]
         public GameCharacterInMatch* inMatch;
 
         [FieldOffset(0x10)]
         public Game* game;
-    }
 
-    [StructLayout(LayoutKind.Explicit)]
-    public unsafe struct Move
-    {
-        // These offsets only work for Battler's c.5A for some reason
-        [FieldOffset(0x3cc)]
-        public byte damage;
+        [FieldOffset(0x524)]
+        public Sequence* _5;
 
-        [FieldOffset(0x3cd)]
-        public byte knockbackType;
+        [FieldOffset(0x528)]
+        public Sequence* _6to4;
 
-        [FieldOffset(0x3ce)]
-        public byte prop3;
+        [FieldOffset(0x52C)]
+        public Sequence* _5to2;
 
-        [FieldOffset(0x3cf)]
-        public byte hitEffect;
+        [FieldOffset(0x530)]
+        public Sequence* _2;
+
+        [FieldOffset(0x534)]
+        public Sequence* _3to1;
+
+        [FieldOffset(0x538)]
+        public Sequence* _2to5;
+
+        [FieldOffset(0x53C)]
+        public Sequence* _6;
+
+        [FieldOffset(0x540)]
+        public Sequence* _4;
+
+        [FieldOffset(0x544)]
+        public Sequence* _8;
+
+        [FieldOffset(0x548)]
+        public Sequence* _9;
+
+        [FieldOffset(0x54C)]
+        public Sequence* _7;
+
+        [FieldOffset(0x570)]
+        public Sequence* _66;
+
+        [FieldOffset(0x574)]
+        public Sequence* _44;
+
+        [FieldOffset(0x610)]
+        public Sequence* far5A;
+
+        [FieldOffset(0x614)]
+        public Sequence* close5A;
+
+        [FieldOffset(0x61c)]
+        public Sequence* far5B;
+
+        [FieldOffset(0x620)]
+        public Sequence* close5B;
+
+        [FieldOffset(0x624)]
+        public Sequence* _6B;
+
+        [FieldOffset(0x628)]
+        public Sequence* far5C;
+
+        [FieldOffset(0x62c)]
+        public Sequence* close5C;
+
+        [FieldOffset(0x630)]
+        public Sequence* _6C;
+
+        [FieldOffset(0x634)]
+        public Sequence* _2A;
+
+        [FieldOffset(0x63c)]
+        public Sequence* _2B;
+
+        [FieldOffset(0x640)]
+        public Sequence* _3B;
+
+        [FieldOffset(0x644)]
+        public Sequence* _2C;
+
+        [FieldOffset(0x64c)]
+        public Sequence* j8A;
+
+        [FieldOffset(0x650)]
+        public Sequence* j8B;
+
+        [FieldOffset(0x654)]
+        public Sequence* j8C;
+
+        [FieldOffset(0x658)]
+        public Sequence* j9A;
+
+        [FieldOffset(0x65c)]
+        public Sequence* j9B;
+
+        [FieldOffset(0x65e)]
+        public Sequence* j9C;
+
+        [FieldOffset(0x668)]
+        public Sequence* j2C;
+
+        [FieldOffset(0x6FC)]
+        public Sequence* grab;
+
+        [FieldOffset(0x6FE)]
+        public Sequence* grabWhiff;
+
+        [FieldOffset(0x740)]
+        public Sequence* dizzy;
+
+        [FieldOffset(0x744)]
+        public Sequence* appeal;
+
+        [FieldOffset(0x78C)]
+        public Sequence* attackTouch;
+
+        public static Sequence*[] GetSequences(GameCharacter* character) {
+            var length = 248;
+            var charAddr = new IntPtr(character);
+            var firstSeq = IntPtr.Add(charAddr, 0x524);
+            Sequence*[] sequenceArray = new Sequence*[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                sequenceArray[i] = (Sequence*)*((nint*)IntPtr.Add(firstSeq, 0x4 * i));
+            }
+
+            return sequenceArray;
+        }
     }
 
     [StructLayout(LayoutKind.Explicit)]
@@ -100,6 +207,74 @@ namespace Ougon
         public int matchTimer;
     }
 
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct SpriteInfo
+    {
+        public int width;
+        public int height;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 2)]
+    public struct Hitbox
+    {
+        public short x;
+        public short y;
+        public short w;
+        public short h;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 2, Size = 96)]
+    public struct Frame
+    {
+        public ushort sprite_id;
+        public ushort duration;
+        public ushort unknown02;
+        public short tex_x;
+        public short tex_y;
+
+        public ushort unknown05;
+        public ushort unknown06;
+        public ushort unknown07;
+        public ushort unknown08;
+        public ushort unknown09;
+        public ushort unknown0a;
+        public ushort unknown0b;
+        public ushort unknown0c;
+        public ushort unknown0d;
+
+        public Hitbox attackbox;
+
+        public Hitbox hitbox1;
+        public Hitbox hitbox2;
+        public Hitbox hitbox3;
+
+        public ushort unknown1e;
+        public ushort unknown1f;
+        public ushort unknown20;
+        public ushort unknown21;
+
+        public struct Attack
+        {
+            public byte damage;
+            public byte prop1;
+            public ushort prop2;
+            public ushort prop3;
+            public ushort prop4;
+        }
+
+        public Attack attack;
+
+        public ushort unknown26;
+        public ushort unknown27;
+        public ushort unknown28;
+        public ushort unknown29;
+        public ushort unknown2a;
+        public ushort unknown2b;
+        public ushort unknown2c;
+        public ushort unknown2d;
+        public ushort unknown2e;
+        public ushort unknown2f;
+    }
 
     [StructLayout(LayoutKind.Explicit)]
     public unsafe struct Team
@@ -116,4 +291,72 @@ namespace Ougon
         [FieldOffset(0x550A0)]
         public short* health;
     }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public struct SequenceHeader
+    {
+        [FieldOffset(0x40)]
+        public uint frame_count;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public struct Sequence {
+        [FieldOffset(0x0)]
+        public SequenceHeader header;
+
+        [FieldOffset(0x44)]
+        public Frame frame0;
+
+        [FieldOffset(0x44 + (0x60 * 1))]
+        public Frame frame1;
+
+        [FieldOffset(0x44 + (0x60 * 2))]
+        public Frame frame2;
+
+        [FieldOffset(0x44 + (0x60 * 3))]
+        public Frame frame3;
+
+        [FieldOffset(0x44 + (0x60 * 4))]
+        public Frame frame4;
+
+        [FieldOffset(0x44 + (0x60 * 5))]
+        public Frame frame5;
+
+        [FieldOffset(0x44 + (0x60 * 6))]
+        public Frame frame6;
+
+        [FieldOffset(0x44 + (0x60 * 7))]
+        public Frame frame7;
+
+        [FieldOffset(0x44 + (0x60 * 8))]
+        public Frame frame8;
+
+        public unsafe Frame*[] frames() {
+            var length = 9;
+            Frame*[] frameArray = new Frame*[length];
+
+            fixed (Frame* pFrame0 = &frame0)
+            fixed (Frame* pFrame1 = &frame1)
+            fixed (Frame* pFrame2 = &frame2)
+            fixed (Frame* pFrame3 = &frame3)
+            fixed (Frame* pFrame4 = &frame4)
+            fixed (Frame* pFrame5 = &frame5)
+            fixed (Frame* pFrame6 = &frame6)
+            fixed (Frame* pFrame7 = &frame7)
+            fixed (Frame* pFrame8 = &frame8)
+            {
+                frameArray[0] = pFrame0;
+                frameArray[1] = pFrame1;
+                frameArray[2] = pFrame2;
+                frameArray[3] = pFrame3;
+                frameArray[4] = pFrame4;
+                frameArray[5] = pFrame5;
+                frameArray[6] = pFrame6;
+                frameArray[7] = pFrame7;
+                frameArray[8] = pFrame8;
+            }
+
+            return frameArray;
+        }
+    };
 }
--- Ougon/GUI.cs
+++ Ougon/GUI.cs
@@ -1,8 +1,8 @@
 using Reloaded.Hooks.Definitions;
 using Reloaded.Imgui.Hook;
 using Reloaded.Imgui.Hook.Implementations;
-using Reloaded.Mod.Interfaces;
 using DearImguiSharp;
+using TextCopy;
 
 namespace Ougon.GUI
 {
@@ -54,13 +54,86 @@ namespace Ougon.GUI
                         var color = player == null ? 0 : player.characters[i].color;
 
                         if (ImGui.TreeNodePtr(i, $"{name} ({color})")) {
-                            ImGui.Text($"Address: {new IntPtr(character).ToString("x")}");
+                            RenderPointer("", character);
                             ImGui.InputInt($"Health##{playerIndex}-{i}", ref character->inMatch->health, 0, 1, 0);
                             ImGui.InputFloat($"Meter##{playerIndex}-{i}", ref character->inMatch->meter, 0.0f, 0.0f, "%.3f", 0);
                             ImGui.InputFloat($"Stun##{playerIndex}-{i}", ref character->inMatch->stun, 0.0f, 0.0f, "%.3f", 0);
 
                             ImGui.InputFloat($"Position X##{playerIndex}-{i}", ref character->inMatch->positionX, 0.0f, 0.0f, "%.3f", 0);
                             ImGui.InputFloat($"Position Y##{playerIndex}-{i}", ref character->inMatch->positionY, 0.0f, 0.0f, "%.3f", 0);
+
+                            if (ImGui.TreeNodePtr(1, "Moves"))
+                            {
+                                RenderSequence("Idle", playerIndex, 1, character->_5);
+
+                                RenderSequence("6->4", playerIndex, 2, character->_6to4);
+                                RenderSequence("5->2", playerIndex, 3, character->_5to2);
+
+                                RenderSequence("Crouching", playerIndex, 4, character->_2);
+
+                                RenderSequence("3->1", playerIndex, 5, character->_3to1);
+                                RenderSequence("2->5", playerIndex, 6, character->_2to5);
+
+                                RenderSequence("Walk forward", playerIndex, 7, character->_6);
+                                RenderSequence("Walk backward", playerIndex, 8, character->_4);
+
+                                RenderSequence("Neutral jump", playerIndex, 9, character->_8);
+                                RenderSequence("Forward jump", playerIndex, 10, character->_9);
+                                RenderSequence("Backward jump", playerIndex, 11, character->_7);
+
+                                RenderSequence("Dash", playerIndex, 12, character->_66);
+                                RenderSequence("Backdash", playerIndex, 13, character->_44);
+
+                                if (ImGui.TreeNodePtr(14, "A")) {
+                                    RenderSequence("Far 5A", playerIndex, 1, character->far5A);
+                                    RenderSequence("Close 5A", playerIndex, 2, character->close5A);
+                                    RenderSequence("2A", playerIndex, 3, character->_2A);
+                                    RenderSequence("8A", playerIndex, 4, character->j8A);
+                                    RenderSequence("9A", playerIndex, 5, character->j9A);
+
+                                    ImGui.TreePop();
+                                }
+
+                                if (ImGui.TreeNodePtr(15, "B")) {
+                                    RenderSequence("Far 5B", playerIndex, 1, character->far5B);
+                                    RenderSequence("Close 5B", playerIndex, 2, character->close5B);
+                                    RenderSequence("6B", playerIndex, 3, character->_6B);
+                                    RenderSequence("2B", playerIndex, 4, character->_2B);
+                                    RenderSequence("8B", playerIndex, 5, character->j8B);
+                                    RenderSequence("9B", playerIndex, 6, character->j9B);
+
+                                    ImGui.TreePop();
+                                }
+
+                                if (ImGui.TreeNodePtr(16, "C")) {
+                                    RenderSequence("Far 5C", playerIndex, 1, character->far5C);
+                                    RenderSequence("Close 5C", playerIndex, 2, character->close5C);
+                                    RenderSequence("6C", playerIndex, 3, character->_6C);
+                                    RenderSequence("2C", playerIndex, 4, character->_2C);
+                                    RenderSequence("8C", playerIndex, 5, character->j8C);
+                                    RenderSequence("9C", playerIndex, 6, character->j9C);
+
+                                    ImGui.TreePop();
+                                }
+
+                                RenderSequence("Grab", playerIndex, 17, character->grab);
+                                RenderSequence("Grab whiff", playerIndex, 18, character->grabWhiff);
+                                RenderSequence("Dizzy", playerIndex, 19, character->dizzy);
+                                RenderSequence("Appeal", playerIndex, 20, character->appeal);
+                                RenderSequence("Attack Touch", playerIndex, 21, character->attackTouch);
+
+                                if (ImGui.TreeNodePtr(17, "Unknown")) {
+                                    int sequenceIndex = 0;
+                                    foreach (var sequence in GameCharacter.GetSequences(character)) {
+                                        RenderSequence($"Unknown 0x{new IntPtr(sequence).ToString("x")} {(0x524 + (0x4 * sequenceIndex)).ToString("x")}", playerIndex, sequenceIndex, sequence);
+                                        sequenceIndex += 1;
+                                    }
+
+                                    ImGui.TreePop();
+                                }
+
+                                ImGui.TreePop();
+                            }
 
                             ImGui.TreePop();
                         }
@@ -70,6 +143,79 @@ namespace Ougon.GUI
 
                     ImGui.TreePop();
                 }
+        }
+
+        private static unsafe void RenderPointer(string label, void* pointer) {
+            var address = new IntPtr(pointer).ToString("x");
+            var displayString = $"{label} 0x{new IntPtr(pointer).ToString("x")}";
+            var size = new ImVec2();
+            size.X = 100;
+            size.Y = 20;
+            if (ImGui.Button(displayString, size)) {
+                ClipboardService.SetTextAsync(address);
+            }
+        }
+
+        private static unsafe void RenderSequence(string name, int playerIndex, int i, Sequence* sequence)
+        {
+            if (ImGui.TreeNodePtr(i, name))
+            {
+                RenderPointer("", sequence);
+                var frameI = 0;
+                var damage = 0;
+
+                foreach (var frame in sequence->frames())
+                {
+                    if (ImGui.TreeNodePtr(frameI, $"Frame {frameI}"))
+                    {
+                        int durationInt = frame->duration;
+                        ImGui.InputInt($"Duration##{playerIndex}-{name}-{frameI}", ref durationInt, 0, 1, 0);
+                        frame->duration = (ushort)durationInt;
+
+                        int damageInt = frame->attack.damage * 3;
+                        ImGui.InputInt($"Damage##{playerIndex}-{name}-{frameI}", ref damageInt, 0, 1, 0);
+                        frame->attack.damage = (byte)(damageInt / 3);
+
+                        RenderHitbox("Attackbox", name, playerIndex, frameI, 1, ref frame->attackbox);
+                        RenderHitbox("Hurtbox 1", name, playerIndex, frameI, 2, ref frame->hitbox1);
+                        RenderHitbox("Hurtbox 2", name, playerIndex, frameI, 3, ref frame->hitbox2);
+                        RenderHitbox("Hurtbox 3", name, playerIndex, frameI, 4, ref frame->hitbox3);
+
+                        if (damageInt > 0)  {
+                            damage += damageInt;
+                        }
+
+                        ImGui.TreePop();
+                    }
+
+                    frameI += 1;
+                }
+
+                ImGui.Text($"Damage: {damage} {damage * 3}");
+
+                ImGui.TreePop();
+            }
+        }
+
+        private static unsafe void RenderHitbox(string name, string moveName, int playerIndex, int frameI, int i, ref Hitbox hitbox)
+        {
+
+            if (ImGui.TreeNodePtr(i, name)) {
+                int attackbox1x = hitbox.x;
+                int attackbox1y = hitbox.y;
+                int attackbox1w = hitbox.w;
+                int attackbox1h = hitbox.h;
+                ImGui.InputInt($"X##{playerIndex}-{name}-{moveName}-{frameI}-{i}x", ref attackbox1x, 0, 1, 0);
+                ImGui.InputInt($"Y##{playerIndex}-{name}-{moveName}-{frameI}-{i}y", ref attackbox1y, 0, 1, 0);
+                ImGui.InputInt($"Width##{playerIndex}-{name}-{moveName}-{frameI}-{i}w", ref attackbox1w, 0, 1, 0);
+                ImGui.InputInt($"Height##{playerIndex}-{name}-{moveName}-{frameI}-{i}h", ref attackbox1h, 0, 1, 0);
+                hitbox.x = (short)attackbox1x;
+                hitbox.y = (short)attackbox1y;
+                hitbox.w = (short)attackbox1w;
+                hitbox.h = (short)attackbox1h;
+
+                ImGui.TreePop();
+            }
         }
 
         private unsafe void RenderDebugWindow()
@@ -90,7 +236,7 @@ namespace Ougon.GUI
             {
                 if (ImGui.CollapsingHeaderBoolPtr("Current Match", ref isDefaultOpen, 0))
                 {
-                    ImGui.Text($"Match Address: {new IntPtr(_context.match).ToString("x")}");
+                    RenderPointer("Match:", _context.match);
 
                     {
                         ImGui.BeginGroup();
